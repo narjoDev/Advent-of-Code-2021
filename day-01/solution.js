99d0da17e42681cd0645aa8a6fb69ddd1f12cea7
narjoDev <63086420+narjoDev@users.noreply.github.com>
2021-12-05 14:55:15 -0800

    Complete day 1 part 2 (#1)
    * Add day 1 part 2 solution, partially tested
    
    * Add more documentation
    
    Co-authored-by: narjoDev <narjoDev@users.noreply.github.com>

--- day-01/solution.js
+++ day-01/solution.js
@@ -31,14 +31,19 @@ function splitStringByLines(string) {
   return string.split(lineEndPattern);
 }
 
+/**
+ * Takes a file name and returns an array of numbers converted from the file's lines.
+ * @param {string} inputFileName File name to input data from.
+ * @returns {Array<Number>} Array of numbers imported from file.
+ */
 function importPuzzleInput(inputFileName) {
   return convertArrayToNumbers(splitStringByLines(readTextFile(inputFileName)));
 }
 
 /**
- * Returns the number of elements greater than the previous element in an array
- * @param {Array} array
- * @returns {number} Number of elements greater than the previous element
+ * Returns the number of elements greater than the previous element in an array.
+ * @param {Array} array Array of comparable elements.
+ * @returns {number} Number of elements greater than the previous element.
  */
 function countIncreasesInArray(array) {
   let increases = 0;
@@ -55,7 +60,7 @@ function countIncreasesInArray(array) {
  * Returns an array with elements converted to numbers.
  * Converts only truthy elements (and 0), removing NaN.
  * @param {Array} array
- * @returns {Array<Number>} Array with truthy (and 0) elements converted to numbers
+ * @returns {Array<Number>} Array with truthy (and 0) elements converted to numbers.
  */
 function convertArrayToNumbers(array) {
   const result = array.reduce((newArray, currentValue) => {
@@ -69,3 +74,29 @@ function convertArrayToNumbers(array) {
 }
 
 console.log(countIncreasesInArray(importPuzzleInput(INPUT_FILE)));
+console.log(countIncreasesInArray(importPuzzleInput("test-from-prompt.txt")));
+
+// Consider sums of a three-measurement sliding window.
+// How many sums are larger than the previous sum?
+
+/**
+ * Compares the sums of elements in a rolling window,
+ * returning the number of windows greater than the previous.
+ * @param {Array} array Array of comparable elements.
+ * @param {number} windowSize Size of the rolling window to compare. Defaults to 1.
+ * @returns {number} Number of windows with greater sums than the previous windows.
+ */
+function countWindowIncrease(array, windowSize = 1) {
+  let increases = 0;
+  let previous = Array(windowSize);
+  for (let i = 0; i < array.length; i++) {
+    const thisElement = array[i];
+    if (thisElement > previous.shift()) increases++;
+    previous.push(thisElement);
+  }
+  return increases;
+}
+
+console.log(countWindowIncrease(importPuzzleInput(INPUT_FILE), 3)); // 1395
+console.log(countWindowIncrease(importPuzzleInput("test-from-prompt.txt"), 3)); // 5
+console.log(countWindowIncrease(importPuzzleInput("test-ad-hoc.txt"), 3)); //
