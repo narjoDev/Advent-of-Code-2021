64a469823d2a548ac4b5d404acab2b312727f616
narjoDev <narjoDev@users.noreply.github.com>
2022-01-03 14:04:50 -0800

    Add day template

---
+++ day-template/solution.js
@@ -0,0 +1,11 @@
+"use strict";
+/* jshint 
+esversion: 6,
+devel: true,
+node: true,
+-W097 
+*/
+
+const { readFile, splitByLines } = require("../libs/get-input");
+
+module.exports = {};
---
+++ day-template/solution.test.js
@@ -0,0 +1,10 @@
+"use strict";
+/* jshint 
+esversion: 6,
+devel: true,
+node: true,
+-W097 
+*/
+
+const { test, expect, describe } = require("@jest/globals");
+const {} = require("./solution");
