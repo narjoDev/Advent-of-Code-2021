cbfa8ee10cef33b64609612e9431ed423d30c275
narjoDev <narjoDev@users.noreply.github.com>
2022-01-12 17:24:09 -0800

    Add day 8 starting files

---
+++ day-08/solution.js
@@ -0,0 +1,16 @@
+"use strict";
+/* jshint 
+esversion: 6,
+devel: true,
+node: true,
+-W097 
+*/
+
+const { readFile, splitByLines } = require("../libs/get-input");
+
+const FILES = {
+  example: "./day-08/example-input.txt",
+  input: "./day-08/puzzle-input.txt",
+};
+
+module.exports = { FILES };
